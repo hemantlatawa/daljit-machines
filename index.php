d81d10b29c4042be920dafe7403b01bc0f61609a
Hemant Latawa <49094732+hemantlatawa@users.noreply.github.com>
2021-08-20 18:10:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE HTML>
+<!--
+	Aesthetic by gettemplates.co
+	Twitter: http://twitter.com/gettemplateco
+	URL: http://gettemplates.co
+-->
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Heavy Duty and Light Duty Industrial Machines Manufacturer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Information about leading manufacturer and suppliers of Industrial Machines in India" />
+	<meta name="keywords" content="Lathe, Slotting, Drilling, Bandsaw, Bandsaw, Machines" />
+	<meta name="author" content="gettemplates.co" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content="Heavy Duty and Light Duty Industrial Machines Manufacturer"/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content="Industrial Machines"/>
+	<meta property="og:description" content="Information about leading manufacturer and suppliers of Industrial Machines in India"/>
+	<meta name="twitter:title" content="Heavy Duty and Light Duty Industrial Machines Manufacturer" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400|Montserrat:400,700" rel="stylesheet"> -->
+
+	<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="gtco-loader"></div>
+	
+	<div id="page">
+
+		<nav class="gtco-nav" role="navigation">
+			<div class="container">
+				
+				<div class="row">
+					<div class="col-sm-4 col-xs-2">
+						<div id="gtco-logo"><a href="index.html">Industrial Machines</div>
+					</div>
+					<div class="col-xs-8 text-right fh5co-top-social">
+						<ul class="gtco-social">
+							<li><a href="https://www.facebook.com/daljitmachines" class="icon-facebook"></i></a></li>
+							<li><a href="https://twitter.com/daljitmachines" class="icon-twitter"></i></a></li>
+							<li><a href="https://www.instagram.com/daljitmachines" class="icon-instagram"></i></a></li>
+							<li><a href="https://www.pinterest.com/daljit_machines" class="icon-pinterest"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</nav>
+		
+		<div id="gtco-intro">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1 text-center">
+						<div class="dt js-height">
+							<div class="dtc animate-box">
+								<h2 class="gradient-text">Design & Development of Industrial Machines, suited to their products.</h2>
+								<p><a href="https://www.daljitmachines.com/contact-us/" class="btn btn-gradient">Contact Us</a></p>
+							</div>	
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="gtco-project">
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-7">
+						<h3>Products</h3>
+						<p class="desc">A quality tested machines from Indian Machine Tools Manufacturer gives you many advantages. So avail all the benefits of speed, low maintenance, and lightweight. Talk to our experts and bring the perfect slotting machine to your business..</p>
+					</div>
+				</div>
+				<div class="row row-pb-md">
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com/lathe-machines/"><img src="images/img_3.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Lathe Machine</h3>
+						</a>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com/slotting-machine/"><img src="images/img_4.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Slotting Machine</h3>
+						</a>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com/hacksaw-machine/"><img src="images/img_2.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Hacksaw Machine</h3>
+						</a>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com/horizontal-surface-grinder"><img src="images/img_1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Horizontal Surface Grinder</h3>
+						</a>
+					</div>
+					
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com/drilling-machines/"><img src="images/img_5.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Drilling Machine</h3>
+						</a>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com/milling-machines/"><img src="images/img_6.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Milling Machine</h3>
+						</a>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com"><img src="images/img_9.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Welding MachineS</h3>
+						</a>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com/horizontal-band-saws/"><img src="images/img_8.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Bandsaw Machine</h3>
+						</a>
+					</div>
+					<div class="col-md-4 col-sm-4 col-xs-6 fh5co-project animate-box">
+						<a href="https://www.daljitmachines.com/shaper-machine/"><img src="images/img_7.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+							<h3>Shaper Machine</h3>
+						</a>
+					</div>
+					
+				</div>
+				
+			</div>
+		</div>
+	
+
+		<footer id="fh5co-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="fh5co-footer-widget">
+							<p>3539 - New Janta Nagar, Opp. Power house Street No. 7, Gill Road, Ludhiana - 141003 </p>
+						</div>
+					</div>
+					<div class="col-md-3 col-md-push-1">
+						<div class="fh5co-footer-widget">
+							<p><a href="tel://+91-98786-09009">+91-98786-09009, 85349-00009</a> <br> <a href="#">sales@daljitmachines.com</a></p>
+						</div>
+					</div>
+					<div class="col-md-4 col-md-push-1">
+						<div class="fh5co-footer-widget gtco-social-wrap">
+							<ul class="gtco-social">
+								<li><a href="https://www.facebook.com/daljitmachines" class="icon-facebook"></i></a></li>
+							<li><a href="https://twitter.com/daljitmachines" class="icon-twitter"></i></a></li>
+							<li><a href="https://www.instagram.com/daljitmachines" class="icon-instagram"></i></a></li>
+							<li><a href="https://www.pinterest.com/daljit_machines" class="icon-pinterest"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
